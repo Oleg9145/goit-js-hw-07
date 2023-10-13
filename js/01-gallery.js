c3dc2ed5619ee84a4d0112479323e8d1f9d7ff19
Oleg9145 <oleg.zinenko94@gmail.com>
2023-10-13 17:37:24 +0200

    Завдання 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const parentElement = document.querySelector(".gallery");
+
+const galleryHTML = galleryItems
+  .map(
+    (item) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>
+`
+  )
+  .join("");
+
+parentElement.insertAdjacentHTML("beforeend", galleryHTML);
+
+parentElement.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains("gallery__image")) {
+    const largeImageURL = event.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+      <img src="${largeImageURL}" width="500" height="300">
+    `);
+    instance.show();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const parentElement = document.querySelector(".gallery");
+
+const galleryHTML = galleryItems
+  .map(
+    (item) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+       <img class="gallery__image" src=" ${item.preview}" alt="${item.description}" />
+    </a>
+ </li>
+`
+  )
+  .join("");
+
+parentElement.insertAdjacentHTML("beforeend", galleryHTML);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionType: "data-alt",
+  captionsData: "alt",
+});
+parentElement.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains("gallery__image")) {
+  }
+});
